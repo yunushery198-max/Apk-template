97d2e91500f4617a145ed57189361884c49a86e5
yunushery198-max <Yunushery198@gmail.com>
2025-08-21 17:34:09 +0800

    update project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Apk-template Project
---
+++ app/src/main/java/com/example/apptemplate/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.apptemplate;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.simple_list_item_1);
+    }
+}
